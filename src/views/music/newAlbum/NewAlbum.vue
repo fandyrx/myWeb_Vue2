488d05e307767cde8417629713bd4669c0eff109
Fandy <290104314@qq.com>
2022-10-29 23:20:35 +0800

    修复，新增封装page-search base-ui 网易云登录

--- src/views/music/newAlbum/NewAlbum.vue
+++ src/views/music/newAlbum/NewAlbum.vue
@@ -1,18 +1,18 @@
 <template>
-	<div>
-		<ComNav title="新碟上架" />
-		<AlbumItem v-bind="$attrs" />
-	</div>
+  <div>
+    <ComNav title="新碟上架" />
+    <AlbumItem v-bind="$attrs" />
+  </div>
 </template>
 
 <script>
-import ComNav from "@/components/content/comNav/ComNav.vue";
+import ComNav from "@/components/content/commend/comNav/ComNav.vue";
 import AlbumItem from "./c-cpns/AlbumItem.vue";
 export default {
-	name: "NewAlbum",
-	components: {
-		ComNav,
-		AlbumItem,
-	},
+  name: "NewAlbum",
+  components: {
+    ComNav,
+    AlbumItem,
+  },
 };
 </script>
